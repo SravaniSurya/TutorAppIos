b1c70d156847a038bedfff75184e681c3ed686b4
Surya Sravani Bommakanti <suryasravanibommakanti@Suryas-Laptop.ht.home>
2024-08-01 00:41:31 -0400

    parent class addition

---
+++ Tutor App/Parent/ParentDashboard.swift
@@ -0,0 +1,18 @@
+//
+//  ParentDasborad.swift
+//  Tutor App
+//
+//  Created by Midhun on 01/08/24.
+//
+
+import SwiftUI
+
+struct ParentDashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParentDashboard()
+}
